823b7c9bec3a0bc5430469f0e60a4dea481d7cb5
Ale1078 <alessandro.pachera@edu.immaginazioneelavoro.it>
2026-03-04 14:20:43 +0100

    a

--- Lezione2/01_Variabili.py
+++ Lezione2/01_Variabili.py
@@ -17,3 +17,22 @@ temperatura = 21.5
 
 print(float(pi))
 print(float(temperatura))
+
+calcolo = 0.1 + 0.2
+print(round(calcolo,2))
+
+nomeUser = "Anna"
+emailUser = "anna@gmail.com"
+etàUser = "22"
+corso = "Tecnico Informatico"
+
+print(nomeUser + " - " + emailUser)
+print(nomeUser, "-", emailUser, "-", etàUser, "anni")
+print(f"{nomeUser} - email:{emailUser} - età:{etàUser}")
+
+Mionome = "Alessandro"
+Miocognome = "Pachera"
+Miaetà = "18"
+Miocorso = "Tecnico Informatico"
+
+print(f"{Mionome} - {Miocognome} - {Miaetà} - {Miocorso}")
